5c96aed0acd055deb98e86cd6717b9af4736cada
Shun Huang <zsh@shunsvineyard.info>
2022-11-12 15:39:39 -0800

    Fix fomatting.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Python Project Template'
-copyright = '2022, Shun Huang'
-author = 'Shun Huang'
-release = '0.0.1'
+project = "Python Project Template"
+copyright = "2022, Shun Huang"
+author = "Shun Huang"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -18,13 +18,12 @@ extensions = ["sphinx.ext.napoleon"]
 
 napoleon_google_docstring = False
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'bizstyle'
-html_static_path = ['_static']
+html_theme = "bizstyle"
+html_static_path = ["_static"]
