a6a80515ca33f08d5d1090ce5988e8b67a0a49e1
Shun <shun@formosa1544.com>
2025-05-23 20:46:30 -0700

    Code cleanup.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 project = "Python Project Template"
 copyright = "2022, Shun Huang"
 author = "Shun Huang"
@@ -14,7 +19,7 @@ release = "0.0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.napoleon"]
+extensions = ["sphinx_copybutton", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 napoleon_google_docstring = False
 
